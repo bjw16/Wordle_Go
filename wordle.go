8d051667baa36a785c94fe83b7849cc79f69c903
memesparky <memesparky@users.noreply.github.com>
2025-09-20 13:24:06 -0400

    Created game menu. Added map of int[string] to save player attempts

--- wordle.go
+++ wordle.go
@@ -9,8 +9,48 @@ var wordList = []string{
 	"LUNGS",
 	"TWIST",
 }
+var result = map[int]string {
+
+	1: "[ ][ ][ ][ ][ ]",
+	2: "[ ][ ][ ][ ][ ]",
+	3: "[ ][ ][ ][ ][ ]",
+	4: "[ ][ ][ ][ ][ ]",
+	5: "[ ][ ][ ][ ][ ]",
+	6: "[ ][ ][ ][ ][ ]",
+}
+
 func main(){
-	for _, x := range wordList{
+	var GameMode int
+	printIntro()
+	for true{
+		printGameMode()
+		fmt.Scan(&GameMode)
+		if GameMode == 1{
+			//play game
+			playGame()
+		} else if GameMode == 2{
+			//show stats
+		} else if GameMode == 0{
+			//error or exit
+			break
+		}
+	}
+}
+
+func printIntro(){
+	//game_loop
+	fmt.Println("WORDLE_GO")
+	fmt.Println("Enter options below:")
+}
+
+func printGameMode(){
+	fmt.Println("1 - Play Wordle")
+	fmt.Println("2 - Streaks")
+}
+
+func playGame() {
+	for _, x := range result{
 		fmt.Println(x)
 	}
+	fmt.Println(result)
 }
