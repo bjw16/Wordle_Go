b9e63fa6fb7605425929aa349655f1510ebd2948
Brandon James Whyte <brandonjwhyte@gmail.com>
2025-09-20 21:32:51 -0400

    Just trying to get this 2d array to print

--- wordle.go
+++ wordle.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
 	// "math/rand"
 )
 
@@ -12,30 +14,34 @@ var wordList = []string{
 	"LUNGS",
 	"TWIST",
 }
-var gameResults map[int]string
-var streak int = 0
 
+// var streak int = 0
 var Turns int
+var letterCount int
+var gameResults [][]string
 
-func init(){
-	gameResults = make(map[int]string)
-	for i := 0 ; i < Turns; i++ {
-		gameResults [i] =  "[ ][ ][ ][ ][ ]"
+func init() {
+	Turns = 6
+	letterCount = 5
+	gameResults := make([][]string, Turns, 100)
+	for i := range gameResults {
+		gameResults[i] = append(gameResults[i], "     ")
 	}
 }
 
-func main(){
+func main() {
 	var GameMode int
 	Turns = 6
-	for true{
+	for true {
 		printIntro()
 		fmt.Scan(&GameMode)
-		if GameMode == 1{
+		if GameMode == 1 {
 			//play game
 			playGame()
-		} else if GameMode == 2{
+		} else if GameMode == 2 {
 			//show stats
-		} else if GameMode == 0{
+			fmt.Println(gameResults[0])
+		} else if GameMode == 0 {
 			//error or exit
 			break
 		}
@@ -43,7 +49,7 @@ func main(){
 	}
 }
 
-func printIntro(){
+func printIntro() {
 	//game_loop
 	fmt.Println("")
 	fmt.Println("WORDLE_GO")
@@ -55,15 +61,36 @@ func printIntro(){
 	fmt.Print("> ")
 }
 
-func printGameMode(){
-	
+func playGame() {
+	//randomizes word
+	wordAnswer := wordList[rand.Intn(len(wordList))]
+	//sets how many turns the player should play. Default 6.
+	var gameTurns int = len(wordList)
+
+	//print turn
+	for x := 0; x < gameTurns; x++ {
+		var guess string
+		printTurn(x)
+		fmt.Print("> ")
+		fmt.Scan(&guess)
+		fmt.Println("")
+		if guess == wordAnswer {
+			break
+		}
+	}
+
 }
 
-func playGame() {
-	// wordAnswer := wordList[rand.Intn(len(wordList))]
-	// var gameTurns int = len(wordList)
-	// for x := 0; x < gameTurns; x++{
-	// 	fmt.Println(gameResults[x])
+// print results for all turns so far
+func printTurn(currTurn int) {
+	fmt.Println("Turn: " + strconv.Itoa(currTurn+1))
+	fmt.Println(gameResults[1])
+	// for i := 0; i < Turns; i++ {
+	// 	fmt.Println(gameResults[i])
+	// 	// for v2 := range gameResults[v1] {
+	// 	// 	fmt.Print("[")
+	// 	// 	fmt.Print(v2)
+	// 	// 	fmt.Print("]")
+	// 	// }
 	// }
-	// fmt.Println(wordAnswer)
 }
